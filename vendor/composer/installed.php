908fb9ec62d011248bdcf0d580b1470c9cc4d4a5
Kauã da Silva de Mattos <108145993+Kawatos@users.noreply.github.com>
2024-08-05 12:57:16 -0300

    composer ok

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wdev/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd6a9ea0d4a32ec595c78735d46a9175023548446',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wdev/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd6a9ea0d4a32ec595c78735d46a9175023548446',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
